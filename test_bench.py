d4a3210aa551789eb7948d11d381f1df4d6a5e8b
DikaVer <dika1596@gmail.com>
2024-05-04 16:09:26 +0200

    merged

--- test_bench.py
+++ test_bench.py
@@ -1,29 +1,38 @@
-
 import numpy as np
 import unittest
-from main import run_simulation
+from main import run_saved_simulation
 import src.filters as filters
 #import environment
 from unittest.mock import patch
 import src.agent as robot_agent
+import pickle as pkl
+
 
 class Experiments(unittest.TestCase):
+
     def setUp(self):
         filters.MEASUREMENT_NOISE = np.zeros(3)
-        filters.R = np.zeros((3,3))
-        filters.Q = np.array([[0.1,0,0],[0,0.1,0],[0,0,0.1]])
+        filters.R = np.zeros((3, 3))
+        filters.Q = np.array([[0.1, 0, 0], [0, 0.1, 0], [0, 0, 0.1]])
         robot_agent.HANDLE_SENSORDATA_MEMORIZE = False
+        with open("tracker.pkl", "rb") as f:
+            self.track = pkl.load(f)
 
     def test_measurement_noise(self):
-         filters.MEASUREMENT_NOISE = np.array([0.99,0.63,3.22])
-         episode = {"max_time_steps": 100}
-         run_simulation(delta_t=1, graphGUI=None,episode = episode)
+        filters.MEASUREMENT_NOISE = np.array([0.99, 0.63, 3.22])
+        run_saved_simulation(delta_t=1,
+                             graphGUI=None,
+                             track=self.track,
+                             num_landmarks=8)
 
     def test_sensor_noise(self):
-         episode = {"max_time_steps": 100}
-         robot_agent.HANDLE_SENSORDATA_MEMORIZE
-         filters.R = np.array([[0.99,0,0],[0,0.98,0],[0,0,1.99]])
-         run_simulation(delta_t=1, graphGUI=None,episode = episode)
+        robot_agent.HANDLE_SENSORDATA_MEMORIZE = True
+        filters.R = np.array([[0.99, 0, 0], [0, 0.98, 0], [0, 0, 1.99]])
+        run_saved_simulation(delta_t=1,
+                             graphGUI=None,
+                             track=self.track,
+                             num_landmarks=8)
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite([Experiments('test_measurement_noise'), Experiments('test_sensor_noise')])
